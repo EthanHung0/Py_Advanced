01653c9a5a0296aa18ab8c81ec9c9ec69db9424a
EthanHung0 <aaronoriginal2009@gmail.com>
2025-06-22 19:38:59 +0700

    thhere

--- 4/4.3.py
+++ 4/4.3.py
@@ -6,14 +6,10 @@ class User:
         self._last_name = None
         self._email = None
 
-    __temp_first_name = None
-    __temp_last_name = None
-    __email_format = "{firstname}.{lastname}"
-
     @property
     def first_name(self):
-        if self.email != None:
-            self._first_name = self.email[:len(self._first_name)-1]
+        if self._email != None:
+            self._first_name = self._email[:len(self._first_name)]
         return self._first_name
     @first_name.setter
     def first_name(self,n):
@@ -21,8 +17,8 @@ class User:
 
     @property
     def last_name(self):
-        if self.email != None:
-            self._last_name = self.email[-(len(self._last_name)+10):]
+        if self._email != None:
+            self._last_name = self._email[-(len(self._last_name)+10):-10]
         return self._last_name
     @last_name.setter
     def last_name(self,n):
@@ -30,13 +26,23 @@ class User:
 
     @property
     def email(self):
+        if self._email != None:
+            self._first_name = self._email[:len(self._first_name)]
+        elif self._email != None:
+            self._last_name = self._email[-(len(self._last_name)+10):-10]
         self._email = f"{self._first_name}.{self._last_name}@gmail.com"
         return self._email
-    # @email.setter
-    # def email(self,first,last):
+    @email.setter
+    def email(self,value):
+        first,last = value
+        self._email = f"{first}.{last}@gmail.com"
 
 
 a = User()
 a.first_name = "Ethan"
 a.last_name = "Aaron"
 print(a.email)
+a.email = "Aaron","Ethan"
+print(a.email)
+print(a.first_name)
+print(a.last_name)
